609b9b16eaf0b0d0fb445d5e4c7bf4688e09b9b9
ayanchavand <ayan.chavand15@gmail.com>
2024-08-10 14:49:48 +0530

    Winner Handler

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -4,16 +4,26 @@ const Square = ({value, onSquareClick}) => {
   return <button className="square" onClick={onSquareClick} >{value}</button>
 }
 
+
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i){
     const nextSquare = squares.slice();
-    nextSquare[i] = "X";
+    //check here if something's buuggy lol
+    if(nextSquare[i] || calculateWinner(nextSquare)) return;
+    nextSquare[i] = xIsNext ? "X" : "O";
     setSquares(nextSquare);
+    setXIsNext(!xIsNext);
   }
+
+  let winner = calculateWinner(squares);
+  let status;
+  status = winner ? "Winner: " + winner : "Next player:" + (xIsNext ? "X": "O");
   return(
     <>
+    <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -32,4 +42,24 @@ export default function Board() {
     </>
   );
    
+}
+
+
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for(let i = 0; i < lines.length; i++){
+    const [x,y,z] = lines[i];
+    if(squares[x] && squares[x] == squares[y] && squares[x] == squares[z]) return squares[x];
+  }
+  return;
 }
